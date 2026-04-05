d02df88acafa5475e13e59da358408e09f332726
elouand <95017261+elouand@users.noreply.github.com>
2026-04-05 12:37:46 +0200

    Delete client directory

--- client/PinkPath2/app/src/main/java/com/traveling/TravelingApp.kt
+++
@@ -1,7 +0,0 @@
-package com.traveling
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class TravelingApp : Application()
--- client/PinkPath2/app/src/main/java/com/traveling/domain/model/User.kt
+++
@@ -1,6 +0,0 @@
-package com.traveling.domain.model
-
-data class User(
-    val username: String,
-    val password: String // En situation réelle, on ne stocke jamais le mot de passe en clair
-)
